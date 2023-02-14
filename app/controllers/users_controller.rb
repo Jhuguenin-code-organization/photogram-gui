f156261420c746dafa50014cc5df7aeba4a993db
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-02-14 06:41:50 +0000

    user details looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,21 @@ class UsersController <ApplicationController
     matching_users = User.all
     
     @list_of_users=matching_users.order({:username => :asc})
-    
+
     render({ :template => "users_templates/index.html.erb"})
   end
+
+  def show
+  url_username = params.fetch("path_username")
+
+    matching_username = User.where({ :username => url_username})
+
+@the_user = matching_username.first
+    if @the_user ==nil
+      redirect_to("/") 
+    else 
+    render({ :template => "users_templates/show.html.erb"})
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "application", :action => "homepage"})
   get("/users", { :controller => "users", :action => "index"})
-  get("/users/:path_id", { :controller => "users", :action => "index"})
+  get("/users/:path_username", { :controller => "users", :action => "show"})
 end
