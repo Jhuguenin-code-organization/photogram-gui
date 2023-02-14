751dac13eea855bdc1843d2d067730ea63acfd5e
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-02-14 05:53:58 +0000

    users index mostly works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def homepage
+    render({ :template=> "homepage.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
-
+  get("/", { :controller => "application", :action => "homepage"})
+  get("/users", { :controller => "users", :action => "index"})
+  get("/users/:path_id", { :controller => "users", :action => "index"})
 end
