4f82f6b9e9985d937e7697c15cff9bf1d813b18a
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-02-14 19:49:44 +0000

    Photos details page basically done Need to add delete/update database links and forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-
+#homepage
   get("/", { :controller => "application", :action => "homepage"})
+  #users
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:path_username", { :controller => "users", :action => "show"})
+#photos
+  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:photo_id", { :controller => "photos", :action => "show"})
+
+  #delete
+  get("/delete_photo/:deleted_photo_id", {:controller => "photos", :action => "delete"})
+
 end
